ac746a6257194c17b989ead3e9a3b42161424064
Yang Zaizhou <yangmxfg@qq.com>
2023-02-15 14:01:20 +0800

    finish lab1

---
+++ Lab1/ex5/ex5_32.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#define print_size(x) printf("Size of %s: %u\n", #x, sizeof(x)); 
+
+int main(void) {
+  print_size(char);
+  print_size(short);
+  print_size(int);
+  print_size(long int);
+  print_size(unsigned int);
+  print_size(void*);
+  print_size(size_t);
+  print_size(float);
+  print_size(double);
+  print_size(int8_t);
+  print_size(int16_t);
+  print_size(int32_t);
+  print_size(int64_t);
+  print_size(time_t);
+  print_size(clock_t);
+  print_size(struct tm);
+  print_size(NULL);
+  return 0;
+}
