e0e69805b05bd19150efe6457a27134bb034fd11
yangzzh@shanghaitech.edu.cn <yangmxfg@qq.com>
2023-03-08 21:22:51 +0800

    finish ex2.s

--- Lab4/ex2.s
+++ Lab4/ex2.s
@@ -17,7 +17,7 @@ main:
 
     # Load the address of the "square" function into a1 (hint: check out "la" on the green sheet)
     ### TODO: YOUR CODE HERE ###
-
+la a1 square
 
     # Issue the call to map
     jal ra, map
@@ -36,7 +36,7 @@ main:
     
     # Load the address of the "decrement" function into a1 (should be very similar to before)
     ### TODO: YOUR CODE HERE ###
-
+la a1 decrement
 
     # Issue the call to map
     jal ra, map
@@ -52,7 +52,12 @@ main:
 map:
     # Prologue: Make space on the stack and back-up registers
     ### TODO: YOUR CODE HERE ###
+    addi sp sp -12
+    sw ra 0(sp)
+    sw s0 4(sp)
+    sw s1 8(sp)
 
+maploop:
     beq a0, x0, done # If we were given a null pointer (address 0), we're done.
 
     add s0, a0, x0 # Save address of this node in s0
@@ -63,30 +68,33 @@ map:
 
     # Load the value of the current node into a0
     ### TODO: YOUR CODE HERE ###
-
+lw a0, 0(s0)
     # Call the function in question on that value
     # Hint: Where do we keep track of the function to call? Recall the parameters of "map".
     ### TODO: YOUR CODE HERE ###
-
+jalr a1
     # Store the returned value back into the node
     # Where can you assume the returned value is?
     ### TODO: YOUR CODE HERE ###
-
+sw a0, 0(s0)
     # Load the address of the next node into a0
     # The address of the next node is an attribute of the current node.
     # Think about how structs are organized in memory.
     ### TODO: YOUR CODE HERE ###
-
+    lw a0 ,4(s0)
     # Put the address of the function back into a1 to prepare for the recursion
     ### TODO: YOUR CODE HERE ###
-
+addi a1 s1 0
     # Recurse
     ### TODO: YOUR CODE HERE ###
-
+j maploop
 done:
     # Epilogue: Restore register values and free space from the stack
     ### TODO: YOUR CODE HERE ###
-
+lw ra 0(sp)
+    lw s0 4(sp)
+    lw s1 8(sp)
+    addi sp sp 12
     jr ra # Return to caller
 
 # === Definition of the "square" function ===
